795252be602c08e47306cad9a46f01c47b26052a
tom <thomas.william.powers@gmail.com>
2015-04-11 12:39:45 -0400

    Home Page routing and view setup

--- Laravel/app/Http/routes.php
+++ Laravel/app/Http/routes.php
@@ -1,20 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+/**
+ * Home Page
+ */
+Route::get('/', 'PagesController@home');
 
-Route::get('/', 'WelcomeController@index');
 
-Route::get('home', 'HomeController@index');
-
+/**
+ * Authentication
+ */
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
