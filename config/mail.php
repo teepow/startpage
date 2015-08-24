59bcb60e44b2b37c809c60e16c1488b28223e97e
tom <thomas.william.powers@gmail.com>
2015-08-24 02:53:42 -0400

    change mail settings

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => null, 'name' => null],
+	'from' => ['address' => 'admin@tompowers.xyz', 'name' => 'Tom'],
 
 	/*
 	|--------------------------------------------------------------------------
